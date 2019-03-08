7ada177f60b5cf1f76df45ddf082db1ff1f57cb6
Gina <Anzo@Athanases-MacBook-Pro.local>
2019-03-08 09:55:00 +0100

    Correct des clear

--- model/model2_tsp.py
+++ model/model2_tsp.py
@@ -8,20 +8,23 @@ model = AbstractModel()
 #Ensemble de noeuds
 model.Nodes = Set()
 
+model.V = Set(within = model.Nodes)
+
 #Ensemble d'Arcs
 model.Arcs = Set(within = model.Nodes * model.Nodes)
 
 #Fonction qui retourne l'ensemble des aretes incident a V ( Conv V )
-def ConvV_init(model, V):
-    return [ E for E in model.Arcs if (V in E )for V in model.Nodes ]
+def ConvV_init(model):
+    for V in model.Nodes :
+        return ( E for E in model.Arcs if V in E )
 #Définition convV
-model.ConvV = Set(initialize = ConvV_init)
+model.ConvV = Set(model.V, initialize = ConvV_init)
 
 #définition parametre C 
 model.C = Param(model.Arcs)
 
 #definition x
-model.X = Var(model.Arcs, domain=Binary)
+model.X = Var(model.Arcs, domain=Binary )
 
 #Fonction objective
 def obj_expression(model):
@@ -29,7 +32,7 @@ def obj_expression(model):
 model.OBJ = Objective(rule=obj_expression)
 
 #Définition de Contrainte
-def ax_constraint_rule(model, E):
+def ax_constraint_rule(model):
     # return the expression for the constraint for i
-    return sum( model.X[E] for E in model.ConvV) == 2
+    return sum( model.X[E] for E in model.Arcs) == 2
 model.AxbConstraint = Constraint(model.Arcs, rule=ax_constraint_rule)
